c079f6a38819684087e3919d0ea0ae818df6aa35
Toni Drumev <tonidrumev86@gmail.com>
2023-11-10 20:33:26 +0200

    Added more solutions

---
+++ orm/07_models_inheritance_and_customization/caller.py
@@ -0,0 +1,11 @@
+import os
+import django
+
+# Set up Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
+django.setup()
+
+
+from main_app.models import UserProfile, Message
+
+
